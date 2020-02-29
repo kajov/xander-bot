3a443c65fbc53b86a1b7d90f92e968e8a4462a70
Karolis <bionicus95@gmail.com>
2020-02-29 17:53:35 +0100

    Test-changes

--- bot.py
+++ bot.py
@@ -2,7 +2,9 @@ import discord
 from discord.ext import commands
 import requests
 
-client = commands.Bot(command_prefix='.')
+TOKEN = 'NjgzMjIyOTU0OTA5NDk5NDMw.XlobHQ.rAR8jMCu4Ma49m_WK3Boj14SLO0'
+
+client = commands.Bot(command_prefix = '.')
 
 @client.event
 async def on_ready():
@@ -11,6 +13,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     channels = ["feedback-text"]
+    channels = ["bot-test"]
 
     if str(message.channel) in channels:
 
@@ -21,11 +24,18 @@ async def on_message(message):
             filename = res[2]
             name = filename[10:-5]
 
-            link = r"C:\Users\Stanley\Downloads\Xense" "\\" + str(name) + ".mp3"
+            ## This has to be changed in to linux env. since we host it in docker and make it lightweight
+            
+            link = r"C:\Users\bioni\Downloads\Xense" "\\" + str(name) + ".mp3"
             doc = requests.get(url[5:-3])
             open(link, 'wb').write(doc.content)
 
             channel = client.get_channel(682574846562926634)
             await channel.send(file=discord.File(link))
 
-client.run('NjgzMjIyOTU0OTA5NDk5NDMw.XlobHQ.rAR8jMCu4Ma49m_WK3Boj14SLO0')
+@client.command()
+async def ping():
+    await client.say("Pong!")
+
+
+client.run(TOKEN)
